76ab356829d94ddbaa042dcd92b504d7ce5dba74
Maciej Krüger <mkg20001@gmail.com>
2019-01-20 09:19:01 +0100

    feat: fix nuclear pkg, move functions to different file

--- config/config.sh
+++ config/config.sh
@@ -3,124 +3,7 @@
 OUT="$PWD/ubuntu"
 KEY="A35AD290"
 
-get_repo_clear() {
-  rfile_sucess=false
-  PKG_DATA=$(wget -qO- "$REPO/dists/$DISTR/binary-$arch/Packages" || ex=$?)
-  if [ $ex -ne 0 ] || [ -z "$PKG_DATA" ]; then
-    # try xz instead
-    PKG_DATA=$(wget -qO- "$REPO/dists/$DISTR/binary-$arch/Packages.xz" || ex=$?)
-    if [ $ex -ne 0 ] || [ -z "$PKG_DATA" ]; then
-      return 0
-    else
-      PKG_DATA=$(wget -qO- "$REPO/dists/$DISTR/binary-$arch/Packages.xz" | xzcat)
-      if [ ! -z "$PKG_DATA" ]; then
-        rfile_sucess=true
-      fi
-    fi
-  else
-    rfile_sucess=true
-  fi
-}
-
-add_from_repo() {
-  REPO="$1"
-  DISTR_NAMES="$2"
-  DISTR_CHANNEL="$3"
-  PKG="$4"
-
-  DIST_ITER=false
-
-  if [ -z $DISTR_NAMES ]; then # if empty iterate over all dists
-    DIST_ITER=true
-    DISTR_NAMES="$PPA_DISTS"
-  fi
-
-  REPO_DB=$(echo "$REPO" | sed "s|[^a-z0-9]|-|g" | sed "s|^|CACHE_ppasc_rp_cl_|g")
-
-  log "repo->$PKG: Updating from $REPO"
-  for arch in $ARCHS; do
-    ex=0
-
-    _db_w "$REPO_DB" "last_success_$arch" ""
-
-    for DISTR_NAME in $DISTR_NAMES; do
-      DISTR="$DISTR_NAME/$DISTR_CHANNEL"
-      dr_display="$DISTR_NAME"
-
-      ls=$(_db_r "$REPO_DB" "last_success_$arch") # use newest or latest successful
-
-      done=false
-      firstFail=false
-      secondFail=false
-
-      while ! $done && ! $secondFail; do
-        if $firstFail; then
-          secondFail=true
-        fi
-
-        get_repo_clear
-
-        if ! $rfile_success; then
-          log "repo->$PKG->$dr_display->$arch: Did not find anything for binary-$arch"
-
-          if [ ! -z "$ls" ]; then
-            log "repo->$PKG->$dr_display->$arch: Trying again using $ls"
-            DISTR="$ls/$DISTR_CHANNEL"
-            dr_display="$DISTR_NAME\$$ls"
-            firstFail=true
-          else
-            secondFail=true
-          fi
-        else
-          PKG_JSON=$(echo "$PKG_DATA" | sed "s|\"|\\\"|g" | sed -r "s|^ .+$||g" | sed -r "s|^([A-Z][A-Za-z0-9-]+): (.*)|\"\1\": \"\2\",|g" | sed "s|^$|\"_\":1},{|g" )
-          PKG_JSON="[{$PKG_JSON\"_\":1}]"
-          PKG_URL=$(echo "$PKG_JSON" | jq -r "map(select(.Package == \"$PKG\"))[] | .Filename" | sort -r | head -n 1)
-          if [ -z "$PKG_URL" ]; then
-            log "repo->$PKG->$dr_display->$arch: Found binary-$arch, but did not contain $PKG"
-
-            if [ ! -z "$ls" ]; then
-              log "repo->$PKG->$dr_display->$arch: Trying again using $ls"
-              DISTR="$ls/$DISTR_CHANNEL"
-              dr_display="$DISTR_NAME\$$ls"
-              firstFail=true
-            else
-              secondFail=true
-            fi
-          else
-            PKG_URL="$REPO/$PKG_URL"
-            log "repo->$PKG->$dr_display->$arch: Latest is $PKG_URL"
-            done=true
-            if $DIST_ITER; then
-              if ! $firstFail; then
-                _db_w "$REPO_DB" "last_success_$arch" "$DISTR_NAME"
-              else
-                _db_w "$REPO_DB" "ls_$DISTR_NAME@$arch" "true"
-              fi
-              lsp=$(_db_r "$REPO_DB" "ls_$DISTR_NAME@$arch")
-              if [ ! -z "$lsp" ]; then # if first was $ls then do this to prevent accidental override
-                IGNORE_RM=true
-              fi
-              add_url "$PKG" "$PKG_URL" "$arch" "" "$DISTR_NAME"
-              if $IGNORE_RM && [ -z "$ls" ]; then
-                _db_w "$REPO_DB" "ls_$DISTR_NAME@$arch" ""
-              fi
-              IGNORE_RM=false
-            else
-              add_url "$PKG" "$PKG_URL" "$arch"
-            fi
-          fi
-        fi
-      done
-
-
-      if [ ! -z "$ls" ] && ! $rfile_success; then
-        log "repo->$PKG->$dr_display->$arch: Did not find anything for binary-$arch $DISTR_NAME, trying $ls"
-        get_repo_clear
-      fi
-
-    done
-  done
-}
+. "$(dirname $(readlink -f $0))/_functions.sh"
 
 # Init Repo
 _init
@@ -210,7 +93,7 @@ add_gh_pkg talenet talenet/talenet
 # Other
 add_url multimc "$(curl -s https://multimc.org/ | grep -o "https://files.*deb")" all
 add_gh_pkg lanshare abdularis/lan-share
-add_gh_pkg nuclear nukeop/nuclear
+add_gh_pkg_any nuclear nukeop/nuclear
 
 # Self-compiled stuff
 for url in $(curl -s https://i.mkg20001.io/deb/ | grep -o './.*deb\"' | sed "s|./|https://i.mkg20001.io/deb/|g" | sed 's|"||g'); do
